583a838cfaf2435d054fa6e837936bd9f235f634
shavit shelli <shavit.shelli59@gmail.com>
2025-08-31 09:13:22 +0300

    finished api fetching

--- model/model.js
+++ model/model.js
@@ -2,19 +2,10 @@ class UserGenerator{
   constructor(){
     this._baseUrl = 'https://randomuser.me/api/'
   }
-  _getUsers(numOfUsers){
-      return new Promise((res,rej) => {
-        $.ajax({
-          url: this._baseUrl+`?results=${numOfUsers}`,
-          dataType: 'json',
-          success: function(data){
-            res(data.results)
-          },
-          error: function(xhr, status, error) {
-            rej(error); // ✅ Reject with error
-        }
-        });  
-      })
+  async _getUsers(numOfUsers){
+      return fetch(this._baseUrl+`?results=${numOfUsers}`)
+              .then((res) => res.json())
+              .then((data) => data.results)
   }
 
   async getUsersData(numOfUsers = 0){
@@ -23,7 +14,7 @@ class UserGenerator{
         users = this._arrangeData(users)
         return users
       }catch(e){
-        console.log(e.message)
+        console.log(e)
       }
   }
 
@@ -55,18 +46,8 @@ class QuoteGenerator{
   }
 
   getRandomQuote(){
-    return new Promise((res,rej) =>{
-      $.ajax({
-        url: this._baseUrl,
-        dataType: 'json',
-        success: function(data){
-          res(data)
-        },
-        error: function(xhr, status, error) {
-          rej(error); // ✅ Reject with error
-      }
-      })
-    })
+    return fetch(this._baseUrl)
+              .then((res) => res.json())
   }
 }
 class PokemonGenerator{
@@ -74,19 +55,9 @@ class PokemonGenerator{
     this._baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
   }
 
-  _fetchPokemon(){
-    return new Promise((res,rej) =>{
-      $.ajax({
-        url: this._baseUrl+`${Math.floor(Math.random() * 100) + 1}`,
-        dataType: 'json',
-        success: function(data){
-          res(data)
-        },
-        error: function(xhr, status, error) {
-          rej(error);
-      }
-      })
-    })
+  async _fetchPokemon(){
+    return fetch(this._baseUrl+`${Math.floor(Math.random() * 100) + 1}`)
+              .then((res) => res.json())
   }
 
   async getRandomPokemon(){
@@ -102,39 +73,78 @@ class LoremGenerator{
   }
 
   getLoremText(maxLen=1){
-    return new Promise((res,rej) =>{
-      $.ajax({
-        url: this._baseUrl,
-        dataType: 'json',
-        success: function(data){
-          res(data)
-        },
-        error: function(xhr, status, error) {
-          rej(error);
-      }
-      })
-    })
+    return fetch(this._baseUrl).then((res) => res.json())
   }
 }
 
-const u = new UserGenerator()
-u.getUsersData(7)
-  .then(u => console.log(u))
-const q = new QuoteGenerator()
-q.getRandomQuote()
-  .then(data => console.log(data.quote) )
-const p = new PokemonGenerator()
-p.getRandomPokemon()
-  .then(data => {
-    console.log(data)
-    const name = data.name
-    const image = data.image
-    console.log(name)
-    console.log(image)
-  } )
-const l = new LoremGenerator()
-l.getLoremText(40)
-  .then(data => {
-    data.forEach(d => console.log(d))
+
+class DataFetcher{
+  constructor(){
+
+  }
+
+  async getUserPageData(){
+    const u = new UserGenerator()
+    const q = new QuoteGenerator()
+    const p = new PokemonGenerator()
+    const l = new LoremGenerator()
     
-  } )
+    const data = await Promise.all([u.getUsersData(7),q.getRandomQuote(),p.getRandomPokemon(),l.getLoremText(3)])
+    return data
+  }
+}
+
+/*
+class APIManager {
+    fetch() {
+        return $.get('/data')
+    }
+}
+
+class Renderer {
+    render(dataPromise) {
+        dataPromise.then(function (data) {
+            $("#body").append(`<div>${data}</div>`)
+        })
+    }
+}
+
+const apiManager = new APIManager()
+const renderer = new Renderer()
+
+let initialDataPromise = apiManager.fetch()
+renderer.render(initialDataPromise) //initial page load
+
+$(".some-thing").on("click", function () {
+    let newDataPromise = apiManager.fetch()
+    renderer.render(newDataPromise)
+})
+*/
+
+
+// const u = new UserGenerator()
+// u.getUsersData(7)
+//   .then(u => console.log(u))
+// const q = new QuoteGenerator()
+// q.getRandomQuote()
+//   .then(data => console.log(data.quote) )
+// const p = new PokemonGenerator()
+// p.getRandomPokemon()
+//   .then(data => {
+//     console.log(data)
+//     const name = data.name
+//     const image = data.image
+//     console.log(name)
+//     console.log(image)
+//   } )
+// const l = new LoremGenerator()
+// l.getLoremText(40)
+//   .then(data => {
+//     data.forEach(d => console.log(d)) 
+//   } )
+
+const d = new DataFetcher()
+d.getUserPageData()
+  .then((data) => {
+    console.log(data)
+  })
